1d7bc8e1331288d833ed76ab87a3fee56697699c
Didier Fabert <didier.fabert@gmail.com>
2025-09-23 16:12:08 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/didier13150/gitlabimport
+
+go 1.21.0
+
+require (
+	github.com/didier13150/gitlablib v0.1.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/didier13150/gitlablib"
+)
+
+func main() {
+
+	var inputFile = flag.String("input", ".env", "File which contains vars to import.")
+	var outputFile = flag.String("output", ".gitlab-vars.new.json", "JSON output file which can be imported by glcli.")
+	var verbose = flag.Bool("verbose", false, "Make application more talkative.")
+	var isCsv = flag.Bool("csv", false, "Input file is a CSV")
+
+	if *verbose {
+		log.Print("Verbose mode is active")
+	}
+
+	flag.Usage = func() {
+		fmt.Printf("Usage: " + os.Args[0] + " [options]\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	vars := gitlablib.NewGitlabVar("", "", *verbose)
+	content, err := os.ReadFile(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Start to import %s", *inputFile)
+
+	if *isCsv {
+		//
+		r := csv.NewReader(strings.NewReader(string(content)))
+		for {
+			data, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			var newvar gitlablib.GitlabVarData
+			newvar.Key = data[0]
+			newvar.Value = strings.ReplaceAll(data[1], "\\n", "\n")
+			if data[2] == "all" {
+				newvar.Env = "*"
+			} else {
+				newvar.Env = data[2]
+			}
+			if data[3] == "true" {
+				newvar.IsRaw = true
+			} else {
+				newvar.IsRaw = false
+			}
+			vars.GitlabData = append(vars.GitlabData, newvar)
+		}
+	} else {
+		lines := strings.Split(string(content), "\n")
+
+		for _, line := range lines {
+			if len(line) == 0 {
+				continue
+			}
+			if line[0:1] == "#" {
+				continue
+			}
+			log.Printf("Line: \"%s\"", line)
+
+			data := strings.SplitN(line, "=", 2)
+			var newvar gitlablib.GitlabVarData
+			newvar.Key = data[0]
+			newvar.Value = data[1]
+			newvar.Env = "*"
+			newvar.IsRaw = true
+
+			vars.GitlabData = append(vars.GitlabData, newvar)
+		}
+	}
+
+	log.Printf("Export to %s", *outputFile)
+	json, err := json.MarshalIndent(vars.GitlabData, "", "  ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if *verbose {
+		log.Printf("Try to write %s file", outputFile)
+	}
+	err = os.WriteFile(*outputFile, json, 0644)
+	if err != nil {
+		log.Println("Export to file", *outputFile, err)
+		return
+	}
+}
